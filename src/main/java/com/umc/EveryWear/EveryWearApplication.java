890be02403494652c4cc3c79e424e04efc5ec4ce
junjunseo <jun85862@gmail.com>
2025-12-27 19:00:46 +0900

    chore:yml 파일 구성 설정

---
+++ src/main/java/com/umc/EveryWear/EveryWearApplication.java
@@ -0,0 +1,13 @@
+package com.umc.EveryWear;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EveryWearApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EveryWearApplication.class, args);
+	}
+
+}
