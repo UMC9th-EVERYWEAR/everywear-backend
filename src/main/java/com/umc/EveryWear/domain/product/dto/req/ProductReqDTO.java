4e493866b879b1e4356b5d0c3dc2f1118b66a99b
taerimiiii <shipton0201@gmail.com>
2026-01-17 15:15:40 -0500

    feat: 예시 값 변경

--- src/main/java/com/umc/EveryWear/domain/product/dto/req/ProductReqDTO.java
+++ src/main/java/com/umc/EveryWear/domain/product/dto/req/ProductReqDTO.java
@@ -19,7 +19,7 @@ public class ProductReqDTO {
     public static class ImportMusinsaDTO {
         @NotBlank(message = "상품 URL은 필수입니다.")
         @Pattern(regexp = "^(https://www\\.musinsa\\.com/products/\\d+$|https://musinsa\\.onelink\\.me/[^/]+/[^/]+.*)$", message = "지원되지 않는 url 형식입니다")
-        @Schema(description = "무신사 상품 URL", example = "https://www.musinsa.com/products/5797789")
+        @Schema(description = "무신사 상품 URL", example = "string")
         private String product_url;
     }
 
@@ -30,7 +30,7 @@ public class ProductReqDTO {
     public static class Import29cmDTO {
         @NotBlank(message = "상품 URL은 필수입니다.")
         @Pattern(regexp = "^(https://www\\.29cm\\.co\\.kr/products/\\d+.*|https://29cm\\.onelink\\.me/.*)$", message = "지원되지 않는 url 형식입니다")
-        @Schema(description = "29cm 상품 URL", example = "https://www.29cm.co.kr/products/3401549?categoryLargeCode=268100100&categoryMediumCode=268105100&categorySmallCode=")
+        @Schema(description = "29cm 상품 URL", example = "string")
         private String product_url;
     }
 
@@ -41,7 +41,7 @@ public class ProductReqDTO {
     public static class WconceptImportDTO {
         @NotBlank(message = "상품 URL은 필수입니다.")
         @Pattern(regexp = "^(https://www\\.wconcept\\.co\\.kr/Product/\\d+\\?.*|https://m\\.wconcept\\.co\\.kr/Product/\\d+\\?.*)$", message = "지원되지 않는 url 형식입니다")
-        @Schema(description = "W컨셉 상품 URL", example = "https://www.wconcept.co.kr/Product/306170405?entry_channel=all_category&cate_no=001006&cate_nm=%ED%8C%AC%EC%B8%A0&cate_sort=")
+        @Schema(description = "W컨셉 상품 URL", example = "string")
         private String product_url;
     }
 
@@ -52,7 +52,7 @@ public class ProductReqDTO {
     public static class ImportZigzagDTO {
         @NotBlank(message = "상품 URL은 필수입니다.")
         @Pattern(regexp = "^(https://zigzag\\.kr/catalog/products/\\d+$|https://s\\.zigzag\\.kr/[A-Za-z0-9]+$)", message = "지원되지 않는 url 형식입니다")
-        @Schema(description = "지그재그 상품 URL", example = "https://zigzag.kr/catalog/products/135012008")
+        @Schema(description = "지그재그 상품 URL", example = "string")
         private String product_url;
     }
 }
