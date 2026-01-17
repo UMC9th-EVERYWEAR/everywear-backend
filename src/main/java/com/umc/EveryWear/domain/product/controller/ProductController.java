8e032398d9c7913b79304c7748d2f5237153c38f
taerimiiii <shipton0201@gmail.com>
2026-01-17 16:19:20 -0500

    docs: 스웨거 문서 수정

--- src/main/java/com/umc/EveryWear/domain/product/controller/ProductController.java
+++ src/main/java/com/umc/EveryWear/domain/product/controller/ProductController.java
@@ -23,7 +23,16 @@ public class ProductController {
 
     private final ProductCommandService productCommandService;
 
-    @Operation(summary = "무신사 상품 등록", description = "등록할 상품 url을 받아 DB에 상품 정보를 저장합니다.")
+    @Operation(
+            summary = "무신사 상품 등록",
+            description = "등록할 상품 url을 입력해 상품 정보를 저장합니다.\n\n" +
+                    "입력 URL 형식들 : \n\n" +
+                    " • 웹 URL: https://www.musinsa.com/products/{상품ID}\n\n" +
+                    " • 앱 URL: https://www.musinsa.com/products/{상품ID}\n\n" +
+                    " • 웹 공유하기: https://musinsa.onelink.me/ANAQ/{공유코드}\n\n" +
+                    " • 앱 공유하기: https://musinsa.onelink.me/ANAQ/{공유코드}\n\n" +
+                    " • 쇼핑몰 앱 공유하기: https://musinsa.onelink.me/PvkC/{공유코드}"
+    )
     @PostMapping("/import/musinsa")
     public ApiResponse<ProductResDTO.ImportDTO> importMusinsaProduct(
             @AuthenticationPrincipal Long userId,
@@ -41,7 +50,16 @@ public class ProductController {
         return ApiResponse.onSuccess(successCode, response);
     }
 
-    @Operation(summary = "지그재그 상품 등록", description = "등록할 상품 url을 받아 DB에 상품 정보를 저장합니다.")
+    @Operation(
+            summary = "지그재그 상품 등록",
+            description = "등록할 상품 url을 입력해 상품 정보를 저장합니다.\n\n" +
+                    "입력 URL 형식들 : \n\n" +
+                    " • 웹 URL: https://zigzag.kr/catalog/products/{상품ID}\n\n" +
+                    " • 앱 URL: https://zigzag.kr/catalog/products/{상품ID}\n\n" +
+                    " • 웹 공유하기: https://zigzag.kr/catalog/products/{상품ID}\n\n" +
+                    " • 앱 공유하기: https://zigzag.kr/catalog/products/{상품ID}\n\n" +
+                    " • 쇼핑몰 앱 공유하기: https://s.zigzag.kr/{공유코드}"
+    )
     @PostMapping("/import/zigzag")
     public ApiResponse<ProductResDTO.ImportDTO> importZigzagProduct(
             @AuthenticationPrincipal Long userId,
@@ -59,7 +77,16 @@ public class ProductController {
         return ApiResponse.onSuccess(successCode, response);
         }
 
-    @Operation(summary = "W컨셉 상품 등록", description = "등록할 상품 url을 받아 DB에 상품 정보를 저장합니다.")
+    @Operation(
+            summary = "W컨셉 상품 등록",
+            description = "등록할 상품 url을 입력해 상품 정보를 저장합니다.\n\n" +
+                    "입력 URL 형식들 : \n\n" +
+                    " • 웹 URL: https://www.wconcept.co.kr/Product/{상품ID}?entry_channel=...\n\n" +
+                    " • 앱 URL: https://m.wconcept.co.kr/Product/{상품ID}?applanding=X\n\n" +
+                    " • 웹 공유하기: https://www.wconcept.co.kr/Product/{상품ID}?applanding=Y\n\n" +
+                    " • 앱 공유하기: https://m.wconcept.co.kr/Product/{상품ID}?applanding=Z}\n\n" +
+                    " • 쇼핑몰 앱 공유하기: https://m.wconcept.co.kr/Product/{상품ID}?applanding=Y"
+    )
     @PostMapping("/import/wconcept")
     public ApiResponse<ProductResDTO.ImportDTO> importWconceptProduct(
             @AuthenticationPrincipal Long userId,
@@ -77,7 +104,16 @@ public class ProductController {
         return ApiResponse.onSuccess(successCode, response);
         }
 
-    @Operation(summary = "29cm 상품 등록", description = "등록할 상품 url을 받아 DB에 상품 정보를 저장합니다.")
+    @Operation(
+        summary = "29cm 상품 등록", 
+        description = "등록할 상품 url을 입력해 상품 정보를 저장합니다.\n\n" +
+            "입력 URL 형식들 : \n\n" +
+            " • 웹 URL: https://www.29cm.co.kr/products/{상품ID}?categoryLargeCode=...\n\n" +
+            " • 앱 URL: https://www.29cm.co.kr/products/{상품ID}\n\n" +
+            " • 웹 공유하기: https://29cm.onelink.me/1080201211/{공유코드}\n\n" +
+            " • 앱 공유하기: https://29cm.onelink.me/1080201211/{공유코드}\n\n" +
+            " • 쇼핑몰 앱 공유하기: https://29cm.onelink.me/1080201211/{공유코드}"
+    )
     @PostMapping("/import/29cm")
     public ApiResponse<ProductResDTO.ImportDTO> import29cmProduct(
             @AuthenticationPrincipal Long userId,
