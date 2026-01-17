7862c2ecf93d6c250dbf0703d92b92213873032a
taerimiiii <shipton0201@gmail.com>
2026-01-17 14:47:32 -0500

    fix: 어노테이션 변경

--- src/main/java/com/umc/EveryWear/domain/product/controller/ProductController.java
+++ src/main/java/com/umc/EveryWear/domain/product/controller/ProductController.java
@@ -6,14 +6,12 @@ import com.umc.EveryWear.domain.product.exception.code.ProductSuccessCode;
 import com.umc.EveryWear.domain.product.service.command.ProductCommandService;
 import com.umc.EveryWear.global.apiPayload.ApiResponse;
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.Schema;
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
