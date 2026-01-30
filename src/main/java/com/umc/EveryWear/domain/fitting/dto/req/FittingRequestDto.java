6acb7be87a36f0d65c77a75bede709f1ea2389c8
junjunseo <jun85862@gmail.com>
2026-01-31 08:53:30 +0900

    fix: 피팅 요청 dto productId만 포함하도록 수정

--- src/main/java/com/umc/EveryWear/domain/fitting/dto/req/FittingRequestDto.java
+++ src/main/java/com/umc/EveryWear/domain/fitting/dto/req/FittingRequestDto.java
@@ -3,8 +3,6 @@ package com.umc.EveryWear.domain.fitting.dto.req;
 public class FittingRequestDto {
 
     public record FittingRequest(
-            Long productId,
-            String userImageUrl,
-            String garmentImageUrl
+            Long productId
     ) {}
 }
