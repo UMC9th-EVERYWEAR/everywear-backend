1d79c11245bd7330fc60be05a191e4bb2715fca0
taerimiiii <shipton0201@gmail.com>
2026-01-30 21:18:55 -0500

    refactor: 응답 코드 통일

--- src/main/java/com/umc/EveryWear/domain/product/exception/code/ProductErrorCode.java
+++ src/main/java/com/umc/EveryWear/domain/product/exception/code/ProductErrorCode.java
@@ -10,13 +10,13 @@ import org.springframework.http.HttpStatus;
 public enum ProductErrorCode implements BaseErrorCode {
 
     CRAWLING_FAILED(HttpStatus.BAD_REQUEST,
-            "400",
+            "COMMON400",
             "상품 등록에 실패했습니다."),
     INVALID_URL_FORMAT(HttpStatus.BAD_REQUEST,
-            "400",
+            "COMMON400_2",
             "지원되지 않는 url 형식입니다."),
     PRODUCT_NOT_FOUND(HttpStatus.NOT_FOUND,
-            "404",
+            "COMMON404_1",
             "수정하려는 상품을 찾을 수 없습니다."),
     ;
 
--- src/main/java/com/umc/EveryWear/domain/product/exception/code/ProductSuccessCode.java
+++ src/main/java/com/umc/EveryWear/domain/product/exception/code/ProductSuccessCode.java
@@ -1,6 +1,5 @@
 package com.umc.EveryWear.domain.product.exception.code;
 
-import com.umc.EveryWear.domain.product.enums.ShoppingMall;
 import com.umc.EveryWear.global.apiPayload.code.BaseSuccessCode;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -11,97 +10,69 @@ import org.springframework.http.HttpStatus;
 public enum ProductSuccessCode implements BaseSuccessCode {
 
     MUSINSA_PRODUCT_ADDED(HttpStatus.OK,
-            "200",
+            "COMMON201_1",
             "무신사 상품을 추가했습니다."),
     MUSINSA_PRODUCT_UPDATED(HttpStatus.OK,
-            "200",
+            "COMMON202_1",
             "무신사 상품을 업데이트했습니다."),
     MUSINSA_PRODUCT_URL_UPDATED(HttpStatus.OK,
-            "200",
+            "COMMON202_1",
             "무신사 상품의 URL을 업데이트했습니다."),
     ZIGZAG_PRODUCT_ADDED(HttpStatus.OK,
-            "200",
+            "COMMON201_1",
             "지그재그 상품을 추가했습니다."),
     ZIGZAG_PRODUCT_UPDATED(HttpStatus.OK,
-            "200",
+            "COMMON202_1",
             "지그재그 상품을 업데이트했습니다."),
     ZIGZAG_PRODUCT_URL_UPDATED(HttpStatus.OK,
-            "200",
+            "COMMON202_1",
             "지그재그 상품의 URL을 업데이트했습니다."),
     WCONCEPT_PRODUCT_ADDED(HttpStatus.OK,
-            "200",
+            "COMMON201_1",
             "W컨셉 상품을 추가했습니다."),
     WCONCEPT_PRODUCT_UPDATED(HttpStatus.OK,
-            "200",
+            "COMMON202_1",
             "W컨셉 상품을 업데이트했습니다."),
     WCONCEPT_PRODUCT_URL_UPDATED(HttpStatus.OK,
-            "200",
+            "COMMON202_1",
             "W컨셉 상품의 URL을 업데이트했습니다."),
     CM29_PRODUCT_ADDED(HttpStatus.OK,
-            "200",
+            "COMMON201_1",
             "29cm 상품을 추가했습니다."),
     CM29_PRODUCT_UPDATED(HttpStatus.OK,
-            "200",
+            "COMMON202_1",
             "29cm 상품을 업데이트했습니다."),
     CM29_PRODUCT_URL_UPDATED(HttpStatus.OK,
-            "200",
+            "COMMON202_1",
             "29cm 상품의 URL을 업데이트했습니다."),
     PRODUCTS_RETRIEVED(HttpStatus.OK,
-            "200",
+            "COMMON200",
             "전체 상품을 조회했습니다."),
     TOP_PRODUCTS_RETRIEVED(HttpStatus.OK,
-            "200",
+            "COMMON200",
             "상의 상품을 조회했습니다."),
     BOTTOM_PRODUCTS_RETRIEVED(HttpStatus.OK,
-            "200",
+            "COMMON200",
             "하의 상품을 조회했습니다."),
     OUTER_PRODUCTS_RETRIEVED(HttpStatus.OK,
-            "200",
+            "COMMON200",
             "아우터 상품을 조회했습니다."),
     DRESS_PRODUCTS_RETRIEVED(HttpStatus.OK,
-            "200",
+            "COMMON200",
             "원피스 상품을 조회했습니다."),
     ETC_PRODUCTS_RETRIEVED(HttpStatus.OK,
-            "200",
+            "COMMON200",
             "기타 상품을 조회했습니다."),
+    PRODUCTS_RETRIEVED_EMPTY(HttpStatus.OK,
+            "COMMON204_1",
+            "상품을 조회했습니다. 응답 본문이 없습니다."),
     PRODUCT_LIKE_ENABLED(HttpStatus.OK,
-            "200",
+            "COMMON200",
             "상품 좋아요가 성공적으로 활성화되었습니다."),
     PRODUCT_LIKE_DISABLED(HttpStatus.OK,
-            "200",
+            "COMMON200",
             "상품 좋아요가 성공적으로 비활성화되었습니다."),
     ;
-
-    // 좋아요 토글 API 응답용
-    public static ProductSuccessCode forLikeToggle(boolean isLiked) {
-        return Boolean.TRUE.equals(isLiked) ? PRODUCT_LIKE_ENABLED : PRODUCT_LIKE_DISABLED;
-    }
-
-    // 상품 등록 API 응답용: 쇼핑몰·업데이트 여부에 따른 성공 코드
-    public static ProductSuccessCode forImportResult(ShoppingMall mall, boolean isUrlUpdated, boolean isUpdated) {
-        if (isUrlUpdated) {
-            return switch (mall) {
-                case MUSINSA -> MUSINSA_PRODUCT_URL_UPDATED;
-                case ZIGZAG -> ZIGZAG_PRODUCT_URL_UPDATED;
-                case CM29 -> CM29_PRODUCT_URL_UPDATED;
-                case WCONCEPT -> WCONCEPT_PRODUCT_URL_UPDATED;
-            };
-        }
-        if (isUpdated) {
-            return switch (mall) {
-                case MUSINSA -> MUSINSA_PRODUCT_UPDATED;
-                case ZIGZAG -> ZIGZAG_PRODUCT_UPDATED;
-                case CM29 -> CM29_PRODUCT_UPDATED;
-                case WCONCEPT -> WCONCEPT_PRODUCT_UPDATED;
-            };
-        }
-        return switch (mall) {
-            case MUSINSA -> MUSINSA_PRODUCT_ADDED;
-            case ZIGZAG -> ZIGZAG_PRODUCT_ADDED;
-            case CM29 -> CM29_PRODUCT_ADDED;
-            case WCONCEPT -> WCONCEPT_PRODUCT_ADDED;
-        };
-    }
 
     private final HttpStatus status;
     private final String code;
