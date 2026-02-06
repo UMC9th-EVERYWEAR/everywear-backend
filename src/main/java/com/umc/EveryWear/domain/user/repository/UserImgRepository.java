bb11ee2c5ce27176b4079a9bd771a89efcc9feba
junjunseo <jun85862@gmail.com>
2026-02-06 23:19:55 +0900

    fix: 동시성 문제 해결을 위해 프로필 사진 등록 시 락 걸도록 수정

--- src/main/java/com/umc/EveryWear/domain/user/repository/UserImgRepository.java
+++ src/main/java/com/umc/EveryWear/domain/user/repository/UserImgRepository.java
@@ -2,7 +2,11 @@ package com.umc.EveryWear.domain.user.repository;
 
 import com.umc.EveryWear.domain.user.entity.User;
 import com.umc.EveryWear.domain.user.entity.UserImg;
+import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
@@ -15,7 +19,11 @@ public interface UserImgRepository extends JpaRepository<UserImg, Long> {
 
     List<UserImg> findAllByUser_UserId(Long userId);
 
-    Optional<UserImg> findByUserAndRepresentativeTrue(User user);
-
     Optional<UserImg> findByUser_UserIdAndProfileImageId(Long userId, Long imageId);
+
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("select ui from UserImg ui where ui.user.userId = :userId")
+    List<UserImg> lockAllByUserId(@Param("userId") Long userId);
+
+    boolean existsByUser_UserIdAndRepresentativeTrue(Long userId);
 }
