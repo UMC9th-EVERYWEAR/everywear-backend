5f4ac680d9a4eb9528f97e729136b63023ef60d9
taerimiiii <shipton0201@gmail.com>
2026-02-08 06:55:20 -0500

    fix: 피팅 등록

--- src/main/java/com/umc/EveryWear/domain/fitting/controller/FittingController.java
+++ src/main/java/com/umc/EveryWear/domain/fitting/controller/FittingController.java
@@ -1,11 +1,9 @@
 package com.umc.EveryWear.domain.fitting.controller;
 
-import com.umc.EveryWear.domain.fitting.dto.req.FittingRequestDto;
 import com.umc.EveryWear.domain.fitting.dto.res.FittingResponseDto;
 import com.umc.EveryWear.domain.fitting.exception.code.FittingSuccessCode;
 import com.umc.EveryWear.domain.fitting.service.command.FittingCommandService;
 import com.umc.EveryWear.domain.fitting.service.query.FittingQueryService;
-import com.umc.EveryWear.domain.user.entity.User;
 import com.umc.EveryWear.global.apiPayload.ApiResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
@@ -29,14 +27,14 @@ public class FittingController {
             summary = "가상 피팅 by 임준서(개발 완료)",
             description = "사용자의 대표사진에 상품 사진을 피팅합니다"
     )
-    @PostMapping("/try-on")
+    @PostMapping("/{product_id}/try-on")
     public ApiResponse<FittingResponseDto.FittingApplyResult> requestFitting(
             @AuthenticationPrincipal Long userId,
-            @RequestBody FittingRequestDto.FittingRequest request
+            @PathVariable("product_id") Long productId
     ) {
         FittingResponseDto.FittingApplyResult result = fittingCommandService.requestFitting(
                 userId,
-                request.productId()
+                productId
         );
 
         return ApiResponse.onSuccess(
